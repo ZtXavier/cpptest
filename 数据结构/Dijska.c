208ac1006d940ae5800cf7922c767dc4334dfa91
ZtXavier <1491918111@qq.com>
2021-12-26 15:08:25 +0800

    cpp

---
+++ 数据结构/Dijska.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+#define MAXVEX 9
+#define INDINITY 65535
+typedef int Patharc[MAXVEX];       /* 用于存储最短路径下标的数组 */
--- 数据结构/graph.cpp
+++ 数据结构/graph.cpp
@@ -9,135 +9,6 @@
 
 //用邻接表实现存储
 
-// class my_graph{
-// public:
-//     my_graph(int v)
-//     {
-//         this->v = v;
-//         this->e = 0;
-//         for(int i = 0;i < v;i++)
-//         {
-//             g.push_back(std::vector<int> ());
-//         }
-//     }
-//     // ~my_graph();
-
-//     void my_sort(std::vector<std::vector<int>> &g,int tp)
-//     {
-//         for(int i = 0;i < g[tp].size() - 1;i++)
-//         {
-//             for(int j = 0;j < g[tp].size() - 1 - i;j++)
-//             {
-//                 int tmp;
-//                 if(g[tp][j] < g[tp][j+1])
-//                 {
-//                     tmp = g[tp][j];
-//                     g[tp][j] = g[tp][j+1];
-//                     g[tp][j+1] = tmp;
-//                 }
-//             }
-//         }
-//     }
-    
-//     int has_e(int from,int to)
-//     {
-//         assert(from >= 0 && from < this->v);
-//         assert(to >= 0 && to < this->v);
-//         for (int i = 0;i < g[from].size();i++)
-//         {
-//             if(g[from][i] == to)
-//             {
-//                 return 1;
-//             }
-//         }
-//         return 0;
-//     }
-
-//     void add_e(int from,int to)
-//     {
-//         assert(from >= 0 && from < this->v);
-//         assert(to >= 0 && to < this->v);
-//         g[from].push_back(to);
-//         if(has_e(from,to))
-//         {
-//             g[to].push_back(from);
-//         }
-//         this->e++;
-//     }
-
-//     void print_g()
-//     {
-//         for(int i = 0;i < v;i++)
-//         {
-//             std::cout << i << "的邻接表:";
-//             for(int j = 0;j < g[i].size();j++)
-//             {
-//                 std::cout << g[i][j] << " ";
-//             }
-//             std::cout << std::endl;
-//         }
-//     }
-
-//     void dfs(int n)
-//     {
-//         std::vector<bool> arrived(this->v,false);
-//         std::stack<int> st;
-//         st.push(n);
-//         std::cout << "dfs: ";
-//         while(!st.empty())
-//         {
-//             int tp = st.top();
-//             if(!arrived[tp])
-//             {
-//                 std::cout << tp << " ";
-//                 arrived[tp] = true;
-//             }
-//             st.pop();
-//             my_sort(g,tp);
-//             for(int i = 0;i < g[tp].size();i++)
-//             {
-//                 if(!arrived[g[tp][i]])
-//                 {
-//                     st.push(g[tp][i]);
-//                 }
-//             }
-//         }
-//         std::cout << std::endl;
-//     }
-
-//     void bfs(int n)
-//     {
-//         std::queue<int> qe;
-//         std::vector<bool> arrived(this->v,false);
-//         qe.push(n);
-//          std::cout << "bfs: ";
-//         while(!qe.empty())
-//         {
-//             int tp = qe.front();
-//             if(!arrived[tp])
-//             {
-//                 std::cout << tp << " ";
-//                 arrived[tp] = true;
-//             }
-//             qe.pop();
-//             for(int i = 0;i < g[tp].size();i++)
-//             {
-//                 if(!arrived[g[tp][i]])
-//                 {
-//                     qe.push(g[tp][i]);
-//                 }
-//             }
-//         }
-//         std::cout << std::endl;
-//     }
-
-// private:
-//     int v,e;          //这里两个变量分别代表结点和边
-//     std::vector<std::vector<int>> g;
-// };
-
-//用邻接矩阵的方式存储
-
 class my_graph{
 public:
     my_graph(int v)
@@ -146,53 +17,62 @@ public:
         this->e = 0;
         for(int i = 0;i < v;i++)
         {
-            g.push_back(std::vector<int>(v,0));
+            g.push_back(std::vector<int> ());
         }
     }
-    ~my_graph() = default;
+    // ~my_graph();
+
+    void my_sort(std::vector<std::vector<int>> &g,int tp)
+    {
+        for(int i = 0;i < g[tp].size() - 1;i++)
+        {
+            for(int j = 0;j < g[tp].size() - 1 - i;j++)
+            {
+                int tmp;
+                if(g[tp][j] < g[tp][j+1])
+                {
+                    tmp = g[tp][j];
+                    g[tp][j] = g[tp][j+1];
+                    g[tp][j+1] = tmp;
+                }
+            }
+        }
+    }
     
     int has_e(int from,int to)
     {
         assert(from >= 0 && from < this->v);
         assert(to >= 0 && to < this->v);
-            if(g[from][to] == 1)
+        for (int i = 0;i < g[from].size();i++)
+        {
+            if(g[from][i] == to)
             {
                 return 1;
             }
+        }
         return 0;
     }
 
-    int add_e(int from,int to)
+    void add_e(int from,int to)
     {
         assert(from >= 0 && from < this->v);
         assert(to >= 0 && to < this->v);
+        g[from].push_back(to);
         if(has_e(from,to))
         {
-           return 1;
+            g[to].push_back(from);
         }
-        g[from][to] = 1;
-        g[to][from] = 1;
         this->e++;
-        return 0;
     }
 
     void print_g()
     {
-        std::cout << "矩阵表: " << std::endl;
-
         for(int i = 0;i < v;i++)
         {
-            std::cout << i << "\t";
-        }
-
-        std::cout << "\n\n" << std::endl;
-
-        for(int i = 0;i < v; i++)
-        {
-            
-            for(int j = 0;j < v;j++)
+            std::cout << i << "的邻接表:";
+            for(int j = 0;j < g[i].size();j++)
             {
-                std::cout << g[i][j] << "\t";
+                std::cout << g[i][j] << " ";
             }
             std::cout << std::endl;
         }
@@ -200,7 +80,6 @@ public:
 
     void dfs(int n)
     {
-        assert(n >= 0 && n < this->v);
         std::vector<bool> arrived(this->v,false);
         std::stack<int> st;
         st.push(n);
@@ -214,11 +93,12 @@ public:
                 arrived[tp] = true;
             }
             st.pop();
-            for(int i = v;i >= 0;i--)
+            // my_sort(g,tp);
+            for(int i = 0;i < g[tp].size();i++)
             {
-                if(!arrived[i] && g[tp][i] == 1)
+                if(!arrived[g[tp][i]])
                 {
-                    st.push(i);
+                    st.push(g[tp][i]);
                 }
             }
         }
@@ -227,11 +107,10 @@ public:
 
     void bfs(int n)
     {
-        assert(n >= 0 && n < this->v);
         std::queue<int> qe;
         std::vector<bool> arrived(this->v,false);
         qe.push(n);
-        std::cout << "bfs: ";
+         std::cout << "bfs: ";
         while(!qe.empty())
         {
             int tp = qe.front();
@@ -241,11 +120,11 @@ public:
                 arrived[tp] = true;
             }
             qe.pop();
-            for(int i = 0;i < v;i++)
+            for(int i = 0;i < g[tp].size();i++)
             {
-                if(!arrived[i] && g[tp][i] == 1)
+                if(!arrived[g[tp][i]])
                 {
-                    qe.push(i);
+                    qe.push(g[tp][i]);
                 }
             }
         }
@@ -256,6 +135,127 @@ private:
     int v,e;          //这里两个变量分别代表结点和边
     std::vector<std::vector<int>> g;
 };
+
+//用邻接矩阵的方式存储
+
+// class my_graph{
+// public:
+//     my_graph(int v)
+//     {
+//         this->v = v;
+//         this->e = 0;
+//         for(int i = 0;i < v;i++)
+//         {
+//             g.push_back(std::vector<int>(v,0));
+//         }
+//     }
+//     ~my_graph() = default;
+    
+//     int has_e(int from,int to)
+//     {
+//         assert(from >= 0 && from < this->v);
+//         assert(to >= 0 && to < this->v);
+//             if(g[from][to] == 1)
+//             {
+//                 return 1;
+//             }
+//         return 0;
+//     }
+
+//     int add_e(int from,int to)
+//     {
+//         assert(from >= 0 && from < this->v);
+//         assert(to >= 0 && to < this->v);
+//         if(has_e(from,to))
+//         {
+//            return 1;
+//         }
+//         g[from][to] = 1;
+//         g[to][from] = 1;
+//         this->e++;
+//         return 0;
+//     }
+
+//     void print_g()
+//     {
+//         std::cout << "矩阵表: " << std::endl;
+
+//         for(int i = 0;i < v;i++)
+//         {
+//             std::cout << i << "\t";
+//         }
+
+//         std::cout << "\n\n" << std::endl;
+
+//         for(int i = 0;i < v; i++)
+//         {
+            
+//             for(int j = 0;j < v;j++)
+//             {
+//                 std::cout << g[i][j] << "\t";
+//             }
+//             std::cout << std::endl;
+//         }
+//     }
+
+//     void dfs(int n)
+//     {
+//         assert(n >= 0 && n < this->v);
+//         std::vector<bool> arrived(this->v,false);
+//         std::stack<int> st;
+//         st.push(n);
+//         std::cout << "dfs: ";
+//         while(!st.empty())
+//         {
+//             int tp = st.top();
+//             if(!arrived[tp])
+//             {
+//                 std::cout << tp << " ";
+//                 arrived[tp] = true;
+//             }
+//             st.pop();
+//             for(int i = v;i >= 0;i--)
+//             {
+//                 if(!arrived[i] && g[tp][i] == 1)
+//                 {
+//                     st.push(i);
+//                 }
+//             }
+//         }
+//         std::cout << std::endl;
+//     }
+
+//     void bfs(int n)
+//     {
+//         assert(n >= 0 && n < this->v);
+//         std::queue<int> qe;
+//         std::vector<bool> arrived(this->v,false);
+//         qe.push(n);
+//         std::cout << "bfs: ";
+//         while(!qe.empty())
+//         {
+//             int tp = qe.front();
+//             if(!arrived[tp])
+//             {
+//                 std::cout << tp << " ";
+//                 arrived[tp] = true;
+//             }
+//             qe.pop();
+//             for(int i = 0;i < v;i++)
+//             {
+//                 if(!arrived[i] && g[tp][i] == 1)
+//                 {
+//                     qe.push(i);
+//                 }
+//             }
+//         }
+//         std::cout << std::endl;
+//     }
+
+// private:
+//     int v,e;          //这里两个变量分别代表结点和边
+//     std::vector<std::vector<int>> g;
+// };
 
 
 int main()
