929c75ef4a6f0b733e09b24a0ae662007f339589
ZtXavier <1491918111@qq.com>
2022-11-23 19:43:49 +0800

    hash

--- practice/hashtest.cpp
+++ practice/hashtest.cpp
@@ -7,15 +7,20 @@ using namespace std;
 
 int main() {
     unordered_map<string,int> um(5);
+    cout << um.bucket_count() << endl;
     um.emplace("123",6);
+    const string *key_ptr = &(um.find("123")->first);
     um.emplace("124",7);
     um.emplace("125",8);
     um.emplace("90",66);
     unordered_map<string,int>::iterator it = um.begin();
-    um.emplace("123",6);
-    um.emplace("124",7);
-    um.emplace("125",8);
-    um.emplace("90",66);
-    const string *key_ptr = &(um.find("123")->first);
+    string st = "1";
+    for(int i = 0;i < 10000000;i++){
+        um.insert({st + "1",6});
+
+    }
+    // um.rehash(10000);
+    cout << it->first << endl;
+    cout << *key_ptr << endl;
     return 0;
 }
