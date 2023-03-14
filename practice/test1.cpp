d740aae49bd29cc8f08a0e771072c59ba18ee28e
ZtXavier <1491918111@qq.com>
2023-03-14 23:51:43 +0800

    cppreview

--- practice/test1.cpp
+++ practice/test1.cpp
@@ -1,11 +1,17 @@
 #include<iostream>
-
+#define min(a,b)((a) <= (b) ? (a) : (b))
 
 int main()
 {
-    int *p = 0;      //一个指针指向不确定的地址结果也会不确定
+    // int *p = 0;      //一个指针指向不确定的地址结果也会不确定
     //char a[] = "asfg";
     //char c = a[10];
-    std::cout << *p << std::endl;
+    // std::cout << *p << std::endl;
+    // int a1 = 0;
+    // int a2 = 5;
+    // std::cout << min(a1,a2) << std::endl;
+    int a[5] = {1,2,3,4,5};
+    int *ptr = (int*)(&a+1);
+    printf("%d,%d",*(a+1),*(ptr-1));
     return 0;
 }
---
+++ practice/test4.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+
+ // 虚基类的创建需要派生子类来实现
+// template <typename T> class A {
+//     friend T;
+//     private:
+//         A(){}
+//         ~A(){}
+// };
+
+
+// class B : virtual public A<B> {
+//     public:
+//         B() {}
+//         ~B() {}
+// };
+
+// class C : virtual public B {
+//     public:
+//         C() {}
+//         ~C() {}
+// };
+
+// int main() {
+//     B b;
+//     return 0;
+// }
+using namespace std;
+class A 
+{ 
+ 	virtual void g() 
+ 	{ 
+ 	 	cout << "A::g" << endl; 
+ 	} 
+private: 
+ 	virtual void f() 
+ 	{ 
+ 	 	cout << "A::f" << endl; 
+ 	} 
+}; 
+class B : public A 
+{ 
+ 	void g() 
+ 	{ 
+ 	 	cout << "B::g" << endl; 
+ 	} 
+ 	virtual void h() 
+ 	{ 
+ 	 	cout << "B::h" << endl; 
+ 	} 
+}; 
+typedef void( *Fun )( void ); 
+int main() 
+{ 
+ 	B b; 
+ 	Fun pFun; 
+ 	for(int i = 0 ; i < 3; i++) 
+ 	{ 
+ 	 	pFun = *( Fun* )( ( int *)  ( int* )( &b  + i ) ); 
+ 	 	pFun(); 
+ 	} 	 
+     return 0;
+} 
+
+四个人,手电17 2个人过桥  1 ,1 2 ,2 3 ,5 4 ,10
